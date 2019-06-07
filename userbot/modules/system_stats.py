50cd8020e34e99646faefee6b28805deebe6863d
Ahmet Açıkgöz <43905928+NaytSeyd@users.noreply.github.com>
2019-06-07 14:15:17 +0300

    Some fixes

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -9,6 +9,7 @@ from asyncio import create_subprocess_shell as asyncrunapp
 from asyncio.subprocess import PIPE as asyncPIPE
 from platform import python_version, uname
 from shutil import which
+from os import remove
 
 from telethon import version
 
