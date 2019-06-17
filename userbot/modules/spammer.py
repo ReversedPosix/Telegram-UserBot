dbfb42d60c7ff83892f1ba2e7f5e93dee44ad74d
Ahmet Açıkgöz <43905928+NaytSeyd@users.noreply.github.com>
2019-06-17 11:25:11 +0000

    Some fixes

--- userbot/modules/spammer.py
+++ userbot/modules/spammer.py
@@ -18,6 +18,12 @@ async def tmeme(e):
     for letter in message:
         await e.respond(letter)
     await e.delete()
+    if BOTLOG:
+            await e.client.send_message(
+                BOTLOG_CHATID,
+                "#TSPAM \n\n"
+                "TSpam was executed successfully"
+                )
 
 @register(outgoing=True, pattern="^.spam")
 async def spammer(e):
@@ -27,9 +33,9 @@ async def spammer(e):
         spam_message = str(e.text[8:])
         await asyncio.wait([e.respond(spam_message) for i in range(counter)])
         await e.delete()
-        if LOGGER:
+        if BOTLOG:
             await e.client.send_message(
-                LOGGER_GROUP,
+                BOTLOG_CHATID,
                 "#SPAM \n\n"
                 "Spam was executed successfully"
                 )
@@ -43,9 +49,9 @@ async def bigspam(e):
         for i in range(1, counter):
             await e.respond(spam_message)
         await e.delete()
-        if LOGGER:
+        if BOTLOG:
             await e.client.send_message(
-                LOGGER_GROUP,
+                BOTLOG_CHATID,
                 "#BIGSPAM \n\n"
                 "Bigspam was executed successfully"
                 )
@@ -61,9 +67,9 @@ async def tiny_pic_spam(e):
         for i in range(1, counter):
             await e.client.send_file(e.chat_id, link)
         await e.delete()
-        if LOGGER:
+        if BOTLOG:
             await e.client.send_message(
-                LOGGER_GROUP,
+                BOTLOG_CHATID,
                 "#PICSPAM \n\n"
                 "PicSpam was executed successfully"
                 )
@@ -81,9 +87,9 @@ async def spammer(e):
             await e.respond(spam_message)
             time.sleep(spamDelay)
         await e.delete()
-        if LOGGER:
+        if BOTLOG:
             await e.client.send_message(
-                LOGGER_GROUP,
+                BOTLOG_CHATID,
                 "#DelaySPAM \n\n"
                 "DelaySpam was executed successfully"
                 )
